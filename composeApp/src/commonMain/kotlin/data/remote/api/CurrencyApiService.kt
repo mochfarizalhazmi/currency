5a05f7931565d4920a9451cdfa271160ef2c7d34
Moch. Fariz Al Hazmi <mochfarizalhazmi@gmail.com>
2024-05-25 05:37:45 +0700

    Add CurrencyAPIService interface and CurrencyAPIService implementation

---
+++ composeApp/src/commonMain/kotlin/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,54 @@
+package data.remote.api
+
+import domain.model.ApiResponse
+import domain.model.Currency
+import domain.model.CurrencyApiService
+import domain.model.RequestState
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class CurrencyApiServiceImpl: CurrencyApiService {
+    companion object {
+        const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+        const val API_KEY = "cur_live_aBjhXPfdq8p442Ljntfo8riIehIsY4l2Vs7nFJmI"
+    }
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15000
+        }
+        install(DefaultRequest) {
+            headers {
+                append("apiKey", API_KEY)
+            }
+        }
+    }
+
+    override suspend fun getLatestExchangeRates(): RequestState<List<Currency>> {
+        return try {
+            val response = httpClient.get(ENDPOINT)
+            if (response.status.value == 200) {
+                val apiResponse = Json.decodeFromString<ApiResponse>(response.body())
+                RequestState.Success(data = apiResponse.data.values.toList())
+            } else {
+                RequestState.Error(message = "HTTP Error Code: ${response.status}")
+            }
+        } catch (e: Exception) {
+            RequestState.Error(message = e.message.toString())
+        }
+    }
+}
